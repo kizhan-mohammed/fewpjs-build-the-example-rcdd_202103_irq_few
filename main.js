f950dd343fb5c6095468b41491d7666aa7f99e97
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-05 10:15:17 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const FULL_HEART = '♥'
 
 const errorHide = () => {
   const modal = document.getElementById("modal");
-  modal.classList.add("hidden");
+  // modal.classList.add("hidden");
 }
 errorHide();
 
