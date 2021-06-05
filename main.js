22f2bddd0abee3df9c30e18a39dd23a3eacd50f0
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-05 10:15:53 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const heart = node => {
 }
 
 window.addEventListener("click", e => {
-  if(e.target.textContent === EMPTY_HEART) like(e.target);
+  if(e.target.textContent === EMPTY_HEART) modal(e.target);
 })
 
 
